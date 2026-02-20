e8a1bd68d2845d7e6a57016dbcf77e0a16d28864
Llarkruks <arnau.cepero@estudiantat.upc.edu>
2026-02-20 08:29:26 +0100

    commit funcional

--- 5-exercise/exercise.js
+++ 5-exercise/exercise.js
@@ -6,17 +6,23 @@ fetch('https://jsonplaceholder.typicode.com/users/')
     const evenUsers = users.filter(user => user.id % 2 === 0);
 
     // 2. MAP
-    const cleanUsers = evenUsers.map(({ id, name, address: { city } }) => ({ id, name, city }));
+    const cleanUsers = evenUsers.map(
+      ({ id, name, address: { city } }) => ({ id, name, city })
+    );
 
     // 3. SPREAD
     const guestUser = { id: 0, name: 'Guest User', city: '—' };
     const withGuest = [guestUser, ...cleanUsers];
 
     // 4. REDUCE
-    const totalChars = cleanUsers.reduce((acc, { name }) => acc + name.length, 0);
+    const totalChars = withGuest.reduce(
+      (acc, { name }) => acc + name.length,
+      0
+    );
 
     console.log('--- Processed Users ---');
     console.table(withGuest);
+
     console.log('--- Statistics ---');
     console.log(`Total characters in usernames: ${totalChars}`);
   });
